5ab1ae10b34eb26533f663e6123d26daa5b043cc
Matthias Loitsch <m@tthias.com>
2011-09-14 16:25:38 +0200

    The dropdown now properly displays which option is selected in the option list.

--- formwatcher.DropDown.js
+++ formwatcher.DropDown.js
@@ -1,10 +1,12 @@
 /*  © Matthias Loitsch   */
 
 var DropDown = Class.create({
-  initialize: function(watcher, elements, options) {
+  initialize: function(watcher, elements, options, selectedOptionIdx) {
     this.watcher = watcher;
     this.elements = elements;
     this.options = options;
+    this.selectedOptionIdx = selectedOptionIdx;
+
     elements.get('button').observe('click', function() {
       this.toggleList();
     }.bind(this));
@@ -12,28 +14,41 @@ var DropDown = Class.create({
     elements.get('list').select('a').each(function(link) {
       link.observe('click', this.selectedOption.bind(this, link._optionIdx));
     }, this);
-    
-//    var optionIdx;
-//    this.options.find(function(option, i) {
-//      if (option.value == elements.get('input').getValue()) {
-//        optionIdx = i;
-//        return true;
-//      }
-//    });
-//    if (optionIdx !== null) this.selectedOption(optionIdx);
-    
+
+    this.updateState();
   },
   toggleList: function() {
     this.elements.get('list').toggle();
   },
   selectedOption: function(optionIdx) {
-    var option = this.options[optionIdx];
+    this.selectedOptionIdx = optionIdx;
+    var option = this.getSelectedOption();
     this.elements.get('button').update(option.name);
     this.elements.get('list').hide();
     this.elements.get('input').setValue(option.value);
+    this.updateState();
     Formwatcher.changed(this.elements, this.watcher);
+  },
+  updateState: function() {
+    var option = this.getSelectedOption();
+    if (option) {
+      this.elements.get('button').update(option.name);
+      this.elements.get('list').select('li.selected').invoke('removeClassName', 'selected');
+      this.elements.get('list').select('li').any(function(liElement) {
+        if (liElement.down('a')._optionIdx == this.selectedOptionIdx) {
+          liElement.addClassName('selected');
+          return true;
+        }
+      }, this);
+    }
+  },
+  getSelectedOption: function() {
+    if (this.selectedOptionIdx === null) return null;
+    return this.options[this.selectedOptionIdx];
   }
 });
+
+
 
 Formwatcher.decorators.push(new (Class.create(Formwatcher.Decorator, {
   name: 'DropDown',
@@ -48,43 +63,24 @@ Formwatcher.decorators.push(new (Class.create(Formwatcher.Decorator, {
       type: 'hidden'
     });
 
-    var listElement = new Element('div', {
-      className: 'list'
-    });
-    listElement.hide();
-    var ulElement = new Element('ul');
-    listElement.appendChild(ulElement);
 
     var options = [];
     var buttonText = '';
-    var i = 0;
-    $A(input.options).each(function(option) {
-      if (!input.hasClassName('required') || option.value != '') {
-        var liElement = new Element('li', { });
+    var selectedOptionIdx;
 
-        var linkElement = new Element('a', {
-          href: 'javascript:undefined;'
-        }).update(option.innerHTML);
-        linkElement._value = option.value;
-        linkElement._optionIdx = i;
-
-        liElement.appendChild(linkElement);
-        ulElement.appendChild(liElement);
-      }
+    $A(input.options).each(function(option, i) {
       options[i] = {
         value: option.value, 
         name: option.innerHTML
       };
-      if (option.value == input.getValue()) buttonText = option.innerHTML;
-      i ++;
+      if (option.value == input.getValue()) {
+        selectedOptionIdx = i;
+        buttonText = option.innerHTML;
+      }
     });
 
-
-    var buttonElement = new Element('button', {
-      type: 'button'
-    });
-    buttonElement.update(buttonText);
-
+    var buttonElement = this.createButtonElement(buttonText);
+    var listElement = this.createListElement(options);
 
     var elementsContainer = new Element('div', {
       className: 'drop-down'
@@ -105,9 +101,39 @@ Formwatcher.decorators.push(new (Class.create(Formwatcher.Decorator, {
       list: listElement
     });
 
-    new this.Class(watcher, elements, options);
+    new this.Class(watcher, elements, options, selectedOptionIdx);
 
     return elements;
+  },
+  createButtonElement: function(text) {
+    return new Element('button', {
+      type: 'button'
+    }).update(text);
+  },
+  createListElement: function(options, allowEmpty) {
+    var listElement = new Element('div', {
+      className: 'list'
+    });
+    listElement.hide();
+    var ulElement = new Element('ul');
+    listElement.appendChild(ulElement);
+
+    options.each(function(option, i) {
+      if (allowEmpty || option.value != '') {
+        var liElement = new Element('li', { });
+
+        var linkElement = new Element('a', {
+          href: 'javascript:undefined;'
+        }).update(option.name);
+        linkElement._value = option.value;
+        linkElement._optionIdx = i;
+
+        liElement.appendChild(linkElement);
+        ulElement.appendChild(liElement);
+      }
+    });
+    
+    return listElement;
   },
   Class: DropDown
 })));
