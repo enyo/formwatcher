51ea0f38e0f62d76e4b8ef1aa39c64cad248cca6
Matias Meno <m@tias.me>
2012-05-24 12:30:03 +0200

    Removed all deprecated files and cleaned up the structure a bit.

--- pack.sh
+++ pack.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-for i in lib/*.js; do
-  uglifyjs --max-line-len 200 "$i" > minified/$i;
+docco src/*.coffee
+
+cd lib
+for i in *.js; do
+  echo $i
+  uglifyjs --max-line-len 200 "$i" > ../minified/$i;
 done;
+cd ..
 
 cat "minified/formwatcher.js" > "minified/formwatcher.pack.js"
 cat "minified/formwatcher.validators.js" >> "minified/formwatcher.pack.js"
